4ff5a47b6240f88a789929e55c5fcb6095801da0
Micaela Kloster <108692944+MicaelaKloster@users.noreply.github.com>
2025-05-18 23:03:19 -0300

    Revert "arreglo"

--- backend/src/modules/preguntas/preguntas.module.ts
+++ backend/src/modules/preguntas/preguntas.module.ts
@@ -1,13 +1,21 @@
+// Importación de decoradores y módulos necesarios de NestJS
 import { Module } from '@nestjs/common';
-import { PreguntasController } from './controllers/preguntas.controller';
-import { PreguntasService } from './services/preguntas.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Pregunta } from './entities/pregunta.entity';
-import { Opcion } from '../opciones/entities/option.entity';
+import { TypeOrmModule } from '@nestjs/typeorm'; // Módulo de TypeORM para la integración con la base de datos
+import { Pregunta } from '../encuestas/entities/pregunta.entity'; // Entidad que representa la tabla "Pregunta" en la base de datos
+import { Opcion } from '../encuestas/entities/opcion.entity'; // Entidad que representa la tabla "Opción" en la base de datos
+import { PreguntasService } from '../preguntas/services/preguntas.service'; // Servicio para la lógica de negocio de preguntas
+import { PreguntasController } from '../preguntas/controllers/preguntas.controller'; // Controlador para manejar las rutas relacionadas con preguntas
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pregunta, Opcion])],
+  // Importación de módulos necesarios para este módulo
+  imports: [
+    // Configuración de TypeORM para trabajar con las entidades relacionadas
+    TypeOrmModule.forFeature([Pregunta, Opcion]),
+  ],
+  // Declaración de los controladores que manejarán las rutas de este módulo
   controllers: [PreguntasController],
+  // Declaración de los proveedores que contienen la lógica de negocio
   providers: [PreguntasService],
 })
+// Exportación del módulo de preguntas para que pueda ser utilizado en otros módulos
 export class PreguntasModule {}
