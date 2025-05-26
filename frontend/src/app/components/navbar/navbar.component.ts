4ab6718a19e46109a654b1cac09f1add2e24a1eb
Micaela Kloster <108692944+MicaelaKloster@users.noreply.github.com>
2025-05-26 14:03:26 -0300

    navbar

---
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [ButtonModule, RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
