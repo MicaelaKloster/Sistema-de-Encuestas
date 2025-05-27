95d04d167d1b070e0a5373da0f675974f7069c2e
Micaela Kloster <108692944+MicaelaKloster@users.noreply.github.com>
2025-05-27 10:03:24 -0300

    nuevos estilos y correciones

--- frontend/src/app/components/banner/banner.component.ts
+++ frontend/src/app/components/banner/banner.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-banner',
-  imports: [ButtonModule, RouterModule],
+  imports: [ButtonModule, RouterModule, NavbarComponent],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
