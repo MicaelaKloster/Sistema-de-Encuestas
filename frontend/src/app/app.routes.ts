34774e33a10ec4dbf76f4fd58191f9085b9a168a
Emilia <emiliavandelinde@gmail.com>
2025-05-28 13:58:19 -0300

    Agrego los componentes de respuestas y resultados para comenzar a desarrollarlos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { ComienzoComponent } from './components/comienzo/comienzo.component';
 import { CreacionEncuestaComponent } from './components/creacion-encuesta/creacion-encuesta.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { PresentacionEnlacesComponent } from './components/enlaces/presentacion-enlaces.component'
+import { ResponderEncuestaComponent } from './components/respuestas/responder-encuesta.component'
+import { VisualizarResultadosComponent } from './components/resultados/visualizar-resultados.component'
 
 // Definición de las rutas principales de la aplicación Angular
 export const routes: Routes = [
@@ -17,6 +19,12 @@ export const routes: Routes = [
   {
   path: 'presentacion-enlaces', // Ruta para la presentación de enlaces luego de finalizar una encuesta
   component: PresentacionEnlacesComponent, // Componente que se muestra en la ruta 'presentacion-enlaces'
+  },
+  { path: 'participar/:codigoRespuesta', // Ruta para participar y responder la encuesta
+    component: ResponderEncuestaComponent, // Componente que se muestra en la ruta 'participar/:codigoRespuesta'
+  },
+  { path: 'resultados/:codigoResultados', // Ruta para la visualización de los resultados de la encuesta
+    component: VisualizarResultadosComponent, // Componente que se muestra en la ruta 'resultados/:codigoResultados'
   },
   {
     path: '**', // Ruta comodín (cualquier ruta no definida)
---
+++ frontend/src/app/components/resultados/visualizar-resultados.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizar-resultados',
+  templateUrl: './visualizar-resultados.component.html',
+  styleUrls: ['./visualizar-resultados.component.css'],
+})
+export class VisualizarResultadosComponent {}
